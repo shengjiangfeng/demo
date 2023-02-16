b6a887c5a892ad92529fa5a29bb8c632ced14a2e
shengjiangfeng <max.sheng@splashtop.com>
2023-02-16 19:59:03 +0800

    first

---
+++ src/main/java/com/project/demo/DemoApplication.java
@@ -0,0 +1,17 @@
+package com.project.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+	@GetMapping("hello")
+	public String test(){
+		return "i love java";
+	}
+}
